cfb66aaecf95e7f07569b472e1a9db5372dd752b
Gaeltomaz123 <gaeltomaz@gmail.com>
2025-04-10 13:41:28 -0300

    add comments

--- Escalonador.java
+++ Escalonador.java
@@ -9,34 +9,39 @@ public class Escalonador {
     int quantidade_processos;
     List<Processo> bloqueados = new ArrayList<>();
     int restart = 0;
+    // Cores texto
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
 
+    // Construtor
     public Escalonador(Queue<Processo> ready) {
         this.ready = ready;
         this.quantidade_processos = ready.size();
     }
 
     public void executar() {
+        // Selecionado é o processo running
         Processo selecionado = ready.poll();
+        // Enquanto todos processos não derem exit continua o loop
         while(finalizados.size() != quantidade_processos) {
+            // Incremento do temporizador
             temporizador++;
+            // check é a variavel utilizada para nao contar quando o processo vai para blocked
             int check = 1;
 
             // In_Out
             if(selecionado instanceof Input_output) {
                 int surto_count = ((Input_output) selecionado).getSurto_count();
+                // Enquanto tiver creditos e surto de cpu para cumprir
                 if(surto_count > 0 && selecionado.getCreditos() > 0) {
-                    
                     selecionado.setCreditos(selecionado.getCreditos() - 1);
                     selecionado.setTempo_total_CPU(selecionado.getTempo_total_CPU() - 1);
                     ((Input_output) selecionado).setSurto_count(surto_count - 1);
                     System.out.println(ANSI_BLUE + "Tempo: " + temporizador + " - Processo " + selecionado.getNome() + " - Créditos: " + selecionado.getCreditos() + " - Running" + ANSI_RESET);
-                }
-                else {
+                } else { // Quando terminar ou creditos ou surto de cpu vai para blocked
                     check = 0;
                     ((Input_output) selecionado).setSurto_count(((Input_output) selecionado).getSurto_CPU());
                     bloqueados.add(selecionado);
@@ -47,11 +52,12 @@ public class Escalonador {
 
             // CPU Bound
             else {
+                // Se tiver creditos - running
                 if (selecionado.getCreditos() > 0) {
                     selecionado.setCreditos(selecionado.getCreditos() - 1);
                     selecionado.setTempo_total_CPU(selecionado.getTempo_total_CPU() - 1);
                     System.out.println(ANSI_BLUE + "Tempo: " + temporizador  + " - Processo " + selecionado.getNome() + " - Créditos: " + selecionado.getCreditos() + " - Running" + ANSI_RESET);
-                } else {
+                } else { // Quando acabar vai para a lista de prontos
                     check = 0;
                     ready.add(selecionado);
                     selecionado = ready.poll();
@@ -77,13 +83,14 @@ public class Escalonador {
                 bloqueados.remove(retirado);
             }
 
+            // Print dos processos na lista de prontos
             if(!ready.isEmpty() && check == 1) {
                 for(Processo processo : ready) {
                     System.out.println(ANSI_GREEN + "Tempo: " + temporizador + " - Processo " + processo.getNome() + " - Créditos: " + processo.getCreditos() + " - Ready" + ANSI_RESET);
                 }
             }
 
-            //mudar
+            // Se um processo chegar a 0 creditos, altera ordem  do mesmo para mais a alta, reeordenando todos os outros tambem de forma circular
             if(selecionado.getCreditos() == 0 && selecionado.getTempo_total_CPU() != 0) {
                 restart++;
                 while(selecionado.getOrdem() < quantidade_processos) {
@@ -100,8 +107,7 @@ public class Escalonador {
             }
         
             
-            //RUnning tem que estar com 0 dcreditos
-            // REady tem que estar com 0 creditos
+            // Se os processos que estao ready chegarem a 0 creditos, assim como o processo que está running, distribui mais creditos com base na formula cred = cred/2 + prio
             if(selecionado.getCreditos() == 0 && validoParaRedistribuicao(ready)) {
                 selecionado.setCreditos((selecionado.getCreditos()/2) + selecionado.getPrioridade());
                
@@ -118,6 +124,11 @@ public class Escalonador {
                 selecionado = ready.poll();
             }
 
+            if(check==1) {
+                System.out.println("-----------------------------------------------");
+            }
+
+            // Se processo finalizar seu tempo total de cpu, exit
             if(selecionado.getTempo_total_CPU() == 0) {
                 System.out.println(ANSI_RED + "Tempo: " + (temporizador+1) + " - Processo " + selecionado.getNome() + " - Exit" + ANSI_RESET);
                 finalizados.add(selecionado);
@@ -125,9 +136,12 @@ public class Escalonador {
                     selecionado = ready.poll();
                 }
             }
+
+            
         }
     }
 
+    // Funcao para checar se os processos que estao na lista de ready finalizaram seus creditos
     public boolean validoParaRedistribuicao(Queue<Processo>prontos){
         int count = 0;
         for(Processo processo : prontos) {
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        // Criação de processos
+        // Criação de processos, com uma fila de prioridade que ordena automaticamente por prioridade (numero de creditos), caso igual ordena por ordem.
         Queue<Processo> processos = new PriorityQueue<>(new Prioridade());
         processos.add(new Input_output("A", 2, 5, 6, 1, 3));
         processos.add(new Input_output("B", 3, 10, 6, 2, 3));
